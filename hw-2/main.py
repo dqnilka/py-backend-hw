59bb4cdad0c348151eb99f101ea0463f308250c2
Danila Makarov <57109784+dqnilka@users.noreply.github.com>
2024-10-21 10:11:06 +0300

    test

--- hw-2/main.py
+++ hw-2/main.py
@@ -1,3 +1,22 @@
 from fastapi import FastAPI
+import uvicorn
+import sys
+from prometheus_fastapi_instrumentator import Instrumentator
+sys.path.append('../../../')
 
-app = FastAPI(title="Shop API")
+
+from lecture_2.hw.shop_api.cart import router_cart
+from lecture_2.hw.shop_api.item import router_item
+
+app = FastAPI(
+    docs_url='/docs',
+    openapi_url='/docs.json',
+    title="Shop API",
+)
+
+
+app.include_router(router_cart)
+app.include_router(router_item)
+
+
+Instrumentator().instrument(app).expose(app)
